df63373ba74b7d7605dbfcc9aa03f4c36f5d161c
Muskan Chawla <muskanchawla1960@gmail.com>
2026-01-12 23:36:20 -0600

    Update RunningSum.java
    Added Running Sum code

--- arrays/RunningSum.java
+++ arrays/RunningSum.java
@@ -1,1 +1,13 @@
 My Day 1 : Running Sum Array Problem
+
+
+  class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i = 1; i < nums.length;i++){
+            nums[i] = nums[i-1]+nums[i];
+        }
+        return nums;
+
+        
+    }
+}
