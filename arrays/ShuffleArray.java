66cd7b8145ac8b637e15689613544547d0aa6e53
Muskan Chawla <muskanchawla1960@gmail.com>
2026-01-22 08:56:01 -0600

    Update ShuffleArray.java
    Added the code

--- arrays/ShuffleArray.java
+++ arrays/ShuffleArray.java
@@ -1,1 +1,19 @@
 LeetCode 1470: Shuffle the array
+  //Time Complxity = O(n)
+  //Space Complexity = O(n)
+
+  class ShuffleArray {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[nums.length];
+        int index = 0;
+        for(int i = 0; i < n;i++){
+            ans[index] = nums[i];
+            index++;
+            ans[index] = nums[i+n] ; 
+            index++;    
+              }
+              return ans;
+        
+    }
+    
+}
