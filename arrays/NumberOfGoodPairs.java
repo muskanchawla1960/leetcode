50f2121b3ed88cf09f0858478aca8f7bd1609bfb
Muskan Chawla <muskanchawla1960@gmail.com>
2026-01-22 09:18:30 -0600

    Update NumberOfGoodPairs.java
    Added code and Time & Space Complexity

--- arrays/NumberOfGoodPairs.java
+++ arrays/NumberOfGoodPairs.java
@@ -1,1 +1,18 @@
+LeetCode 1512: Number of good Pairs
+  //Time Complexity = O(n^2)
+  //Space complexity = O(n)
+class NumberOfGoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] == nums[j] && i < j){
+                    count++;
 
+                }
+            }
+        }
+        return count;
+        
+    }
+}
