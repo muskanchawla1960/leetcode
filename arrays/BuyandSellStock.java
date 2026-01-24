c6d7aed4dad842e945230dbda057e004fc0004ad
Muskan Chawla <muskanchawla1960@gmail.com>
2026-01-23 20:59:16 -0600

    Update BuyandSellStock.java
    Added code

--- arrays/BuyandSellStock.java
+++ arrays/BuyandSellStock.java
@@ -1,1 +1,26 @@
+Leetcode 121 best time to by and sell stock
+  //TimeCommplexity = O(n)
+  //SpaceComplexity = O(1)
 
+class BuyandSellStock {
+    public int thirdMax(int[] nums) {
+         int minimumPrice = nums[0];
+        int maxProfit = 0;
+           for (int i = 1; i < nums.length; i++) {
+             if (nums[i] < minimumPrice) {
+                minimumPrice = nums[i];
+            } else {
+                int profit = nums[i] - minimumPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+        
+
+           
+        
+    }
+}
