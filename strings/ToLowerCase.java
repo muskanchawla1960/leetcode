dadb097664f6d2d308f2c5bca1d866360a263bf4
Muskan Chawla <muskanchawla1960@gmail.com>
2026-02-01 23:47:26 -0600

    Create ToLowerCase.java

---
+++ strings/ToLowerCase.java
@@ -0,0 +1,10 @@
+Leetcode question: 709 To Lower Case
+  //Time complexity:O(1)
+  //Space complexity:O(1)
+  
+  class ToLowerCase {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+        
+    }
+}
