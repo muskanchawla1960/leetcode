7d1fe4210cee0cffb3a721df7e1f35232494ae3f
Muskan Chawla <muskanchawla1960@gmail.com>
2026-02-02 05:50:33 -0600

    Create ReverseString.java

---
+++ strings/ReverseString.java
@@ -0,0 +1,19 @@
+Leetcode 344 : Reverse a string
+//Time Complexity: O(n)
+//Space complexity :O(1)
+  class ReverseString {
+    public void reverseString(char[] s) {
+       
+       int left = 0;
+       int right = s.length-1;
+       while(left<right){
+        char temp = s[left];
+        s[left] = s[right];
+        s[right]=temp;
+        left++;
+        right--;
+       }
+        
+    }
+}
+  
