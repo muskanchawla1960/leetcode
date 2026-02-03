a6f0b907494dc22bf33ca7e1ad4d0f53f81bbc5a
Muskan Chawla <muskanchawla1960@gmail.com>
2026-02-03 09:56:04 -0600

    Create GoalParserInterpretation.java
    Created GoalParserInterpretation

---
+++ strings/GoalParserInterpretation.java
@@ -0,0 +1,21 @@
+Leetcode 1678 : Goal Parser Interpretation
+//Time Complexity : O(n)
+//Space Complexity : O(n)
+  class GoalParserInterpretation{
+    public String interpret(String command) {
+        String  result = "";
+        for(int i = 0; i < command.length();i++){
+            if(command.charAt(i) == '(' && command.charAt(i+1) == ')'){
+                result = result + "o" ;
+                i=i+1;
+            }else if(command.charAt(i) == '(' && command.charAt(i+3) == ')'){
+                result = result +"al";
+                i=i+3;
+            }else{
+                result = result + command.charAt(i);
+            }
+        }
+        return result;
+        
+    }
+}
