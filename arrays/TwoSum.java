4f3681a7a86c2b9f9d283e06b9ef7db578ff8c9b
Muskan Chawla <muskanchawla1960@gmail.com>
2026-02-11 12:28:02 -0600

    Update TwoSum.java
    Added code for this problem

--- arrays/TwoSum.java
+++ arrays/TwoSum.java
@@ -1,1 +1,17 @@
 Day 5: Leetcode Solution for Two sum
+  //Time complexity : O(n^2)
+  //Space Complexity : O(1)
+
+  class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
