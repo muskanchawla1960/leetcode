38be276e31ad51ad3f12149d425fb677ac349e7b
Muskan Chawla <muskanchawla1960@gmail.com>
2026-02-13 06:22:51 -0600

    Update ContainsDuplicate.java

--- arrays/ContainsDuplicate.java
+++ arrays/ContainsDuplicate.java
@@ -1,1 +1,19 @@
+class Solution {
+  //Optimized Solution
+  //Time Commplexity : O(n)
+  //Space Complexity :O(n)
+    public boolean containsDuplicate(int[] nums) {
+       HashSet<Integer> set = new HashSet<>();
 
+        for(int num : nums) {
+
+            if(set.contains(num)) {
+                return true;
+            }
+
+            set.add(num);
+        }
+
+        return false;
+    }
+}
