204b6dc03ffdb8b739d00a5145f86b92c3ae1ed5
Muskan Chawla <muskanchawla1960@gmail.com>
2026-02-13 08:07:57 -0600

    Update MoveZeroes.java

--- arrays/MoveZeroes.java
+++ arrays/MoveZeroes.java
@@ -1,1 +1,20 @@
+Leetcode 283 - Move Zeroes
+  //Time Complexity : O(n)
+  //Space complexity : O(1)
+  class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int insertPosition = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i]!=0){
+                nums[insertPosition] = nums[i];
+                insertPosition++;
+            }
 
+        }
+        while(insertPosition <nums.length){
+            nums[insertPosition] = 0;
+            insertPosition++;
+        }
+        
+    }
+}
