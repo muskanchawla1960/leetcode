ac3f7f4473b368f608c4bfa689ec475970b61414
Muskan Chawla <muskanchawla1960@gmail.com>
2026-02-17 07:11:26 -0600

    Update MaximumSubarray.java
    Added Code for maximum subarray sum

--- arrays/MaximumSubarray.java
+++ arrays/MaximumSubarray.java
@@ -1,1 +1,22 @@
+ LeetCode 53 : Maximum Subarray
+  //Time Complexity : O(n)
+  //SpaceComplexity: O(1)
+  //Optimized Solution
+   class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+      int currentSum = 0;
+      int maxSum = Integer.MIN_VALUE;
 
+      for(int i = 0; i < nums.length; i++){
+        currentSum += nums[i];
+        if(currentSum> maxSum){
+            maxSum = currentSum;
+        }
+        if (currentSum < 0){
+            currentSum = 0;
+        }
+      }
+      return maxSum;
+        
+    }
+}
