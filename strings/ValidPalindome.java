f77f006f206871de5981e127e4f5b2e0bd06dc42
Muskan Chawla <muskanchawla1960@gmail.com>
2026-02-19 07:44:48 -0600

    Update ValidPalindome.java

--- strings/ValidPalindome.java
+++ strings/ValidPalindome.java
@@ -1,1 +1,34 @@
+Leetcode 125 : Valid Palindrome
+  //Time Complexity :O(n)
+  //Space Complexity :O(1)
 
+class ValidPalindome {
+    public boolean isPalindrome(String s) {
+         int left = 0;
+        int right = s.length() - 1;
+        boolean isPalindrome = true;
+
+        while (left < right && isPalindrome) {
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+          
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) 
+                != Character.toLowerCase(s.charAt(right))) {
+                isPalindrome = false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return isPalindrome;
+
+            }
+}
