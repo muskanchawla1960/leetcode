84d134b20535f97bb70350a930039a67e900302f
Muskan Chawla <muskanchawla1960@gmail.com>
2026-02-20 18:56:17 -0600

    Update DuplicatesRemoval.java

--- arrays/DuplicatesRemoval.java
+++ arrays/DuplicatesRemoval.java
@@ -1,1 +1,20 @@
+Leetcode 26: Remove Duplicates from Sorted Array
+  //Time complexity :O(n)
+  //Space complexity :O(1)
+  class DuplicateRemoval {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int pos = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] !=nums[i-1]){
+                nums[pos] =nums[i];
+                pos++;
 
+            }
+            
+        }
+        return pos;
+    }
+}
