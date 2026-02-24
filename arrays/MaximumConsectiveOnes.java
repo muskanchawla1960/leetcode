bb3ec7477178265291138d68a15350d27f0d01fd
Muskan Chawla <muskanchawla1960@gmail.com>
2026-02-23 18:09:15 -0600

    Update MaximumConsectiveOnes.java
    Added code

--- arrays/MaximumConsectiveOnes.java
+++ arrays/MaximumConsectiveOnes.java
@@ -1,1 +1,22 @@
+class MaximumConsectiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int currentCount = 0;
+        int maxCount = 0;
 
+        for(int i = 0; i < nums.length; i++) {
+
+            if(nums[i] == 1) {
+                currentCount++;
+                if(currentCount > maxCount) {
+                    maxCount = currentCount;
+                }
+            } else {
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
+    
+        
+    }
+}
