99db97352c8eca3be92c602291ad69b59dfe5dde
Muskan Chawla <muskanchawla1960@gmail.com>
2026-03-09 20:05:08 -0500

    Update ReshapeMatrix.java
    Added Code for this problem

--- 2d-arrays/ReshapeMatrix.java
+++ 2d-arrays/ReshapeMatrix.java
@@ -1,1 +1,30 @@
+Leetcode 566: Reshape the Matrix
 
+  class ReshapeMatrix {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[][] result = mat;
+
+        if(m * n == r * c){
+
+            result = new int[r][c];
+
+            for(int i = 0; i < m * n; i++){
+
+                int oldRow = i / n;
+                int oldCol = i % n;
+
+                int newRow = i / c;
+                int newCol = i % c;
+
+                result[newRow][newCol] = mat[oldRow][oldCol];
+            }
+        }
+
+        return result;
+    }
+}
+
+  
