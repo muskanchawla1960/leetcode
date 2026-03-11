05faaa770f01bbd4c5125f6b45e426030684098d
Muskan Chawla <muskanchawla1960@gmail.com>
2026-03-11 11:39:07 -0500

    Update RobotReturn.java

--- strings/RobotReturn.java
+++ strings/RobotReturn.java
@@ -1,1 +1,25 @@
+Leetcode 657: Robot Return to Origin
+  // Time Complexity : O(n)
+  // Space Complexity :O(1)
+  class RobotReturn{
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y= 0;
+        for(int i = 0; i < moves.length(); i++){
+            char move = moves.charAt(i);
+            if(move == 'R'){
+                x++;
+            }else if(move == 'L'){
+                x--;
+            }
+            else if (move == 'U'){
+                y++;
+            }else{
+                y--;
+            }
+            
 
+        }return x == 0 && y==0;
+        
+    }
+}
