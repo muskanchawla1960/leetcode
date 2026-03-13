a576d2445e10d4d021842bd2195131115d8d0029
Muskan Chawla <muskanchawla1960@gmail.com>
2026-03-13 18:28:07 -0500

    Update TwoSum2.java
    Added the Time complexity, space complexity of the optimized code approach with code

--- TwoPointers/TwoSum2.java
+++ TwoPointers/TwoSum2.java
@@ -1,1 +1,26 @@
+LeetCode 167 : Two Sum II - Input Array Is Sorted
 
+  //Time Complexity : O(n)
+  //Space Complexity : O(1)
+  //Optimized approach :
+  class TwoSum2 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while(left< right){
+            if(numbers[left]+numbers[right] == target){
+                return new int[]{left+1,right+1};
+
+            }
+            else if(numbers[left]+numbers[right]<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return new int[]{-1,-1};
+        
+    }
+}
+  
