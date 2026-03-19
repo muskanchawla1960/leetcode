02e590f3693ff2c4edab88a85d7932ef7bbcfa26
Muskan Chawla <muskanchawla1960@gmail.com>
2026-03-19 04:54:14 -0500

    Update MaxAvgSubarray.java
    Added code and time complexity for this problem

--- SlidingWindow/MaxAvgSubarrat.java
+++ SlidingWindow/MaxAvgSubarrat.java
@@ -1,1 +1,28 @@
+Leetcode 643 : Maximum Average Subarray I
+  // Time Complexity : O(n)
+  // Space Complexity :O(1)
 
+  class MaxAvgSubarray {
+    public double findMaxAverage(int[] nums, int k) {
+        
+         int windowSum = 0;
+
+        for(int i = 0; i < k; i++){
+            windowSum = windowSum + nums[i];
+        }
+
+        int maxSum = windowSum;
+
+        for(int i = k; i < nums.length; i++){
+
+            windowSum = windowSum - nums[i - k];
+            windowSum = windowSum + nums[i];
+
+            if(windowSum > maxSum){
+                maxSum = windowSum;
+            }
+        }
+
+        return (double) maxSum / k;
+    }
+}
