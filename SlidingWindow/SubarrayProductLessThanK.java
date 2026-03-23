c40b7b1d83ad2651ffc6bba7f6f87f7ce2699c18
Muskan Chawla <muskanchawla1960@gmail.com>
2026-03-23 09:58:01 -0500

    Update SubarrayProductLessThanK.java

--- SlidingWindow/SubarrayProductLessThanK.java
+++ SlidingWindow/SubarrayProductLessThanK.java
@@ -1,1 +1,30 @@
+LeetCode 713 : Subarray Product Less Than K
+  //Time Complexity :O(n)
+  //Space Complexity :O(1)
 
+  class SubarrayProductLessThanK {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int count = 0;
+        
+        if (k <= 1) {
+            return 0;
+        }
+
+        int left = 0;
+        int product = 1;
+
+        for (int right = 0; right < nums.length; right++) {
+            product = product * nums[right];
+
+            while (product >= k) {
+                product = product / nums[left];
+                left++;
+            }
+
+            count = count + (right - left + 1);
+        }
+
+        return count;
+        
+    }
+}
