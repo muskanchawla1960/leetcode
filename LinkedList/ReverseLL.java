fc8c9278a70358408182ad27ee6a71180a68738e
Muskan Chawla <muskanchawla1960@gmail.com>
2026-03-24 11:06:51 -0500

    Update ReverseLL.java
    Added code for this problem

--- LinkedList/ReverseLL.java
+++ LinkedList/ReverseLL.java
@@ -1,1 +1,34 @@
 
+LeetCode 206: reverse a linked list
+
+  //Time Complexity : O(n)
+  //Space Complexity : O(1)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class ReverseLL {
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode nextNode;
+
+        while(current!= null){
+            nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+
+        }
+        return prev;
+    }
+}
