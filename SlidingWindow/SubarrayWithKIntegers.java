e8c8504a8cfc5bb7df2bff6800f1b9f8655fce4b
Muskan Chawla <muskanchawla1960@gmail.com>
2026-03-26 05:20:56 -0500

    Update SubarrayWithKIntegers.java
    Code, Time Complexity added

--- SlidingWindow/SubarrayWithKIntegers.java
+++ SlidingWindow/SubarrayWithKIntegers.java
@@ -1,1 +1,33 @@
+992. Subarrays with K Different Integers
+  //Time complexity : O(n)
+  //Space Complexity :O(k)
 
+  
+class SubarrayWithKIntegers {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return helper(nums,k) -helper(nums, k-1);
+        
+    }
+
+    private int helper(int[] nums, int k){
+        int left = 0;
+        int count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int right = 0; right < nums.length; right ++){
+            map.put(nums[right], map.getOrDefault(nums[right],0)+1);
+
+            while(map.size()>k){
+                map.put(nums[left],map.get(nums[left]) - 1);
+                if(map.get(nums[left]) == 0){
+                    map.remove(nums[left]);
+                }
+                left++;
+
+            }
+
+            count += right-left+1;
+
+        }
+        return count;
+    }
+}
