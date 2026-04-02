5ffde8a27c6f68ff9bccee95e76b5b2de0f5901f
Muskan Chawla <muskanchawla1960@gmail.com>
2026-04-02 13:23:46 -0500

    Update MinInRotSortedArray.java
    Added code and Time complexity

--- BinarySearch/MinInRotSortedArray.java
+++ BinarySearch/MinInRotSortedArray.java
@@ -1,1 +1,22 @@
+Leetcode 153: Find Minimum in Rotated Sorted Array
+  // Time Complexity = O(logn)
+  //Space Complexity = O(1)
 
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length -1;
+
+        while(left < right){
+            int mid = left + (right-left)/2;
+
+            if(nums[mid]<= nums[right]){
+                right = mid;
+                
+            }else{
+                left = mid+1;
+            }
+        }
+        return nums[left];
+    }
+}
